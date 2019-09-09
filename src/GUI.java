f22dcfd7b63e93f871a6fb02623a5159c72ff630
Mark Berney <berneydwmark@gmail.com>
2019-09-10 10:32:54 +1200

    Added emptyTerrain generation sans UI

--- src/GUI.java
+++ src/GUI.java
@@ -1,16 +1,23 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUI extends JFrame {
     private final int WINDOW_WIDTH = 900;
     private final int WINDOW_HEIGHT = 600;
 
-    public GUI() {
+    private Terrain currentTerrain = null;
+
+    private JButton addTerrainFromServer;
+
+    private GUI() {
         buildGUI();
     }
 
     private void buildGUI() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         setTitle("abc");
         getContentPane();
@@ -21,10 +28,44 @@ public class GUI extends JFrame {
                 dim.height / 2 - getSize().height / 2
         );
 
+        setControlPanel();
+
         setVisible(true);
+    }
+
+    private void setControlPanel() {
+        JPanel controlPanel = new JPanel();
+        add(controlPanel, BorderLayout.SOUTH);
+
+        addTerrainFromServer = new JButton("Add Terrain From Server");
+        addTerrainFromServer.addActionListener(new ActionListeners());
+        controlPanel.add(addTerrainFromServer);
+    }
+
+    private boolean setCurrentTerrain(Terrain newTerrain) {
+        if (currentTerrain == null) {
+            currentTerrain = newTerrain;
+            return true;
+        } else {
+            JOptionPane.showMessageDialog(null, "Terrain is already set.");
+            return false;
+        }
+    }
+
+    private Terrain getCurrentTerrain() {
+        return currentTerrain;
     }
 
     public static void main(String[] args) {
         new GUI();
+    }
+
+    private class ActionListeners implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == addTerrainFromServer) {
+                setCurrentTerrain(new Terrain());
+            }
+        }
     }
 }
