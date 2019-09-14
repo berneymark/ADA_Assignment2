b27ea7bd50d684f5d2ad00a8a2c0c1b77ff7e1ef
Mark Berney <berneydwmark@gmail.com>
2019-09-14 14:29:32 +1200

    Made Vehicle interface

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,5 +1,10 @@
-public class Vehicle {
-    public Vehicle() {
-        
-    }
+public interface Vehicle {
+    public void setCurrentRow();
+    public void setCurrentColumn();
+
+    public void getCurrentRow();
+    public void getCurrentColumn();
+
+    // changes currentRow and currentColumn
+    public void move();
 }
